2b398d34fcc781a1d588f30920095a71823c6471
@neltic <phi.avatar@gmail.com>
2023-05-14 20:50:34 -0600

    Add comment to loader functions

--- static/js/loader.js
+++ static/js/loader.js
@@ -1,26 +1,28 @@
 "use strict";
 /*
  by @neltic on 2015-01-01
- need jQuery v1.10+
+ update on 2023-05-14
  
  Create a loader message
  */
 var loader = {
     id: 'loader', 				// CSS Id
-    isBusy: false,
     delay: 1000, 				// until this time the message is displayed
+    isBusy: false,
+    // show loading message after delay
     show: function (message) {
-        var $loader = document.getElementById(this.id); 
+        var $loader = document.getElementById(this.id);
+        // add to body de loader DOM 
         if ($loader === null) {
             $loader = document.createElement('div');
             $loader.id = this.id;
             $loader.innerHTML = '<div><p></p></div>';
             document.body.appendChild($loader);
-        }        
+        }
         // create loading message
         var $current = document.createElement('div');
         $current.innerHTML = message.replace(/\n/g, '<br/>');
-        $loader.getElementsByTagName('p')[0].parentElement.insertBefore($current, null);        
+        $loader.getElementsByTagName('p')[0].parentElement.insertBefore($current, null);
         // check is time to... 
         if (!this.isBusy) {
             this.isBusy = true;
@@ -31,11 +33,13 @@ var loader = {
             }, this.delay);
         }
     },
+    // hide loading message
     hide: function () {
         this.isBusy = false;
         loader.fadeOut();
         this.clear();
     },
+    // clear the current message list
     clear: function () {
         var $messages = document.querySelector('#' + this.id + ' > div').getElementsByTagName('div');
         if ($messages.length > 0) {
@@ -44,22 +48,24 @@ var loader = {
             }
         }
     },
+    // helper for loader fade in
     fadeIn: function () {
-        var $loader = document.getElementById(this.id); 
+        var $loader = document.getElementById(this.id);
         $loader.style.opacity = 0;
         $loader.style.display = 'block';
         (function fade() {
-            let val = parseFloat($loader.style.opacity);            
-            if (!((val += 0.1) > 1)) { 
+            let val = parseFloat($loader.style.opacity);
+            if (!((val += 0.1) > 1)) {
                 $loader.style.opacity = val;
                 requestAnimationFrame(fade);
             }
         })();
     },
-    fadeOut: function() {
-        var $loader = document.getElementById(this.id); 
+    // helper for loader fade out
+    fadeOut: function () {
+        var $loader = document.getElementById(this.id);
         $loader.style.opacity = 1;
-        (function fade() {            
+        (function fade() {
             if (($loader.style.opacity -= 0.2) < 0) {
                 $loader.style.display = 'none';
             } else {
